74dabd49e44e603228fdb12b833f3574f133d5e3
csrlab-fudan <96805476+csrlab-fudan@users.noreply.github.com>
2022-01-16 12:31:49 +0800

    Update test.m

--- test.m
+++ test.m
@@ -1,7 +1,7 @@
 % Last modified on Jan. 16, 2022
 % Copyright Communication System Research Laboratory, Fudan University
 % quaternary GCM pair
-[a, b] = gen_quaternary_acm_pair(9, 10); 
+[a, b] = gen_quaternary_gcm_pair(9, 10); 
 close all
 figure;
 stem3(xcorr2(a)+xcorr2(b));
